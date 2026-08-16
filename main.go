69795caaaf7925010a507bc51ba87635b2377c4d
Samarth Gupta <samarth1234gupta@gmail.com>
2026-08-17 00:42:51 +0530

    Learning about channels

--- main.go
+++ main.go
@@ -2,32 +2,44 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	currLine := ""
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			parts := strings.Split(string(data[:n]), "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				currLine += parts[i]
+				ch <- currLine
+				currLine = ""
+			}
+			currLine += parts[len(parts)-1]
+		}
+		if currLine != "" {
+			ch <- currLine
+		}
+	}()
+	return ch
+}
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Println("Error opening the file")
+		fmt.Print("File dont exist")
 	}
-	currLine := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		parts := strings.Split(string(data[:n]), "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			currLine += parts[i]
-			fmt.Printf("read: %s\n", currLine)
-			currLine = ""
-		}
-		currLine += parts[len(parts)-1]
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
-	if currLine != "" {
-		fmt.Printf("read: %s\n", currLine)
-	}
-	f.Close()
 }
